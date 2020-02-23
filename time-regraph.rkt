20e72d0eeff33732c68bbc74f217e0f67a5d875d
oliver <oflatt@gmail.com>
2020-02-23 14:21:45 -0700

    fix up condition for break

--- time-regraph.rkt
+++ time-regraph.rkt
@@ -7,6 +7,8 @@
 (require profile/analyzer)
 
 
+(define rebuilding? (make-parameter #f))
+
 (define rules-exprs-port (open-input-file "./rules.txt"))
 (define rules-in (read rules-exprs-port))
 (define rules-out (read rules-exprs-port))
@@ -42,63 +44,78 @@
 
 (define (run-regraph regraph times node-limit iters-file)
   (define last-i
-    (for/last ([i (range times)]
-               #:break (and (regraph-limit regraph) (>= (regraph-count regraph) (regraph-limit regraph))))
+    (for/or ([i (range times)])
+      (define initial-cnt (regraph-count regraph))
       ((rule-phase rules-in rules-out) regraph)
-      i))
-  (unless rebuilding?
-    (writeln iters-file (+ last-i 1))))
+      (if
+       (< initial-cnt (regraph-count regraph) node-limit)
+       #f
+       i)))
+  (unless (rebuilding?)
+    (displayln (+ last-i 1) iters-file)))
 
 (define (render-regraph-info all-regraphs time-file data order)
-  (writeln (exact->inexact (/(profile-total-time data) (length all-regraphs))) time-file)
-  (for ([node (profile-nodes data)])
-    (cond
-      [(equal? (node-id node) 'merge-egraph-nodes!)
-       (writeln (exact->inexact (/ (node-total node) (length all-regraphs))) merge-time-file)]
-      [(equal? (node-id node) 'egraph-rebuild)
-       (writeln (exact->inexact (/ (node-total node) (length all-regraphs))) rebuild-time-file)])))
+  (display (exact->inexact (/(profile-total-time data) (length all-regraphs))) time-file)
+  (display " " time-file)
+  (display (for/or ([node (profile-nodes data)])
+           (cond
+             [(equal? (node-id node) 'merge-egraph-nodes!)
+              (exact->inexact (/ (node-total node) (length all-regraphs)))]
+             [else #f]))
+         time-file)
+  (displayln (for/or ([node (profile-nodes data)])
+             (cond
+               [(equal? (node-id node) 'egraph-rebuild)
+                (exact->inexact (/ (node-total node) (length all-regraphs)))]
+               [else #f]))
+           time-file))
 
-(define rebuilding? (make-parameter #f))
 (define (folder-string)
   (cond
-    [rebuilding?
+    [(rebuilding?)
      "timing-upwards"]
     [else
      "timing-rebuilding"]))
 
-
 (define (time-suite filename)
   (for ([i (range 8)])
+    (define exprs-name
+      (substring (path->string filename) 0 (- (string-length (path->string filename)) 4)))
     (define node-limit (+ 2500 (* i 2500)))
+    (display "Timing with node limit: ")
+    (displayln (number->string node-limit))
     (define suite-port (open-input-file (build-path "exprs" filename)))
     (define time-file (open-output-file
                        (build-path (current-directory)
                                    (folder-string)
-                                   (string-append (number->string node-limit) " "
-                                                  (path->string filename) "-total"))
+                                   (string-append (number->string node-limit) "-"
+                                                  exprs-name "-total.txt"))
                        #:exists 'replace))
     (define iters-file-out
-      (if rebuilding?
+      (if (rebuilding?)
           #f
           (open-output-file
            (build-path (current-directory)
-                       (string-append (number->string node-limit) " "
-                                      (path->string filename) "-iters"))
+                       (string-append (number->string node-limit) "-"
+                                      exprs-name "-iters.txt"))
            #:exists 'replace)))
       
     (define all-regraphs
       (spawn-all-regraphs suite-port))
     (define iters-file-in
-      (if rebuilding?
+      (if (rebuilding?)
           (open-input-file
            (build-path (current-directory)
-                       (string-append (number->string node-limit) " "
-                                      (path->string filename) "-iters")))
+                       (string-append (number->string node-limit) "-"
+                                      exprs-name "-iters.txt")))
           #f))
 
-    (for ([regraph all-regraphs])
+    (for ([regraph all-regraphs] [i (length all-regraphs)])
+      (display "Regraph " )
+      (displayln (number->string i))
+      (flush-output)
       (define iteration-limit
-        (if rebuilding?
+        (if (rebuilding?)
             (read iters-file-in)
             10000000))
       (profile-thunk
@@ -112,6 +129,10 @@
    #:once-each
    [("-r" "--rebuild") "Time regraph with rebuilding enabled"
                        (rebuilding? #t)])
+  
 
   (for ([expr-file (directory-list (build-path (current-directory) "exprs"))])
+    (displayln "#########################")
+    (display "Timing file: ")
+    (displayln (path->string expr-file))
     (time-suite expr-file)))
