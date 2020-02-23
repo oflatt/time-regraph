446ca86fbb0d4cd1b08feb63691a3fc378353f26
oliver <oflatt@gmail.com>
2020-02-23 08:50:53 -0700

    working on timing options

--- time-rebuilding.rkt
+++ time-rebuilding.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
-(require "main.rkt")
-(require "egraph.rkt")
+(require regraph)
+
 (require (except-in profile profile))
 (require profile/render-text)
 (require profile/analyzer)
@@ -40,23 +40,16 @@
         (cons re (spawn-all-regraphs port))
         empty))
 
-(define (run-math regraph times)
-  (for ([i (range times)]
-        #:break (and (regraph-limit regraph) (>= (regraph-count regraph) (regraph-limit regraph))))
-    ((rule-phase rules-in rules-out) regraph)))
+(define (run-regraph regraph times node-limit iters-file)
+  (define last-i
+    (for/last ([i (range times)]
+               #:break (and (regraph-limit regraph) (>= (regraph-count regraph) (regraph-limit regraph))))
+      ((rule-phase rules-in rules-out) regraph)
+      i))
+  (unless rebuilding?
+    (writeln iters-file (+ last-i 1))))
 
-
-(define (run-all-regraphs regraphs iters)
-  (for ([regraph regraphs])
-    (run-math regraph iters)))
-
-
-
-(define time-file (open-output-file "./rebuild-total.txt" #:exists 'replace))
-(define merge-time-file (open-output-file "./rebuild-merge.txt" #:exists 'replace))
-(define rebuild-time-file (open-output-file "./rebuild-rebuild.txt" #:exists 'replace))
-
-(define (render-regraph-info all-regraphs data order)
+(define (render-regraph-info all-regraphs time-file data order)
   (writeln (exact->inexact (/(profile-total-time data) (length all-regraphs))) time-file)
   (for ([node (profile-nodes data)])
     (cond
@@ -64,32 +57,61 @@
        (writeln (exact->inexact (/ (node-total node) (length all-regraphs))) merge-time-file)]
       [(equal? (node-id node) 'egraph-rebuild)
        (writeln (exact->inexact (/ (node-total node) (length all-regraphs))) rebuild-time-file)])))
-(define NUM_ITERATIONS 4)
 
-(println "Math timing")
-(for ([i (range 4)])
-  (println i)
-  (profile-thunk
-   (lambda () (run-all-regraphs all-regraphs-math 1))
-   #:render (curry render-regraph-info all-regraphs-math)))
+(define rebuilding? (make-parameter #f))
+(define (folder-string)
+  (cond
+    [rebuilding?
+     "timing-upwards"]
+    [else
+     "timing-rebuilding"]))
+
 
 (define (time-suite filename)
-  (define file-port (open-input-file filename))
-  (define time-file (open-output-file (build-path (current-directory)
-                                                  "timing"
-                                                  (string-append filename "-total"))))
-  (define merge-file (open-output-file (build-path (current-directory)
-                                                  "timing"
-                                                  (string-append filename "-merge"))))
-  (define upwards-file (open-output-file (build-path (current-directory)
-                                                  "timing"
-                                                  (string-append filename "-upwards"))))
-  (define all-regraphs
-    (spawn-all-regraphs file-port)))
+  (for ([i (range 8)])
+    (define node-limit (+ 2500 (* i 2500)))
+    (define suite-port (open-input-file (build-path "exprs" filename)))
+    (define time-file (open-output-file
+                       (build-path (current-directory)
+                                   (folder-string)
+                                   (string-append (number->string node-limit) " "
+                                                  (path->string filename) "-total"))
+                       #:exists 'replace))
+    (define iters-file-out
+      (if rebuilding?
+          #f
+          (open-output-file
+           (build-path (current-directory)
+                       (string-append (number->string node-limit) " "
+                                      (path->string filename) "-iters"))
+           #:exists 'replace)))
+      
+    (define all-regraphs
+      (spawn-all-regraphs suite-port))
+    (define iters-file-in
+      (if rebuilding?
+          (open-input-file
+           (build-path (current-directory)
+                       (string-append (number->string node-limit) " "
+                                      (path->string filename) "-iters")))
+          #f))
 
-(for ([i (range NUM_ITERATIONS)])
-  (println i)
-  (profile-thunk
-   (lambda () (run-all-regraphs all-regraphs-hamming 1))
-   #:render (curry render-regraph-info all-regraphs-hamming)))
+    (for ([regraph all-regraphs])
+      (define iteration-limit
+        (if rebuilding?
+            (read iters-file-in)
+            10000000))
+      (profile-thunk
+       (lambda () (run-regraph regraph iteration-limit node-limit iters-file-out))
+       #:render (curry render-regraph-info (list regraph) time-file)))))
+     
 
+(module+ main
+  (command-line 
+   #:program "time-rebuilding"
+   #:once-each
+   [("-r" "--rebuild") "Time regraph with rebuilding enabled"
+                       (rebuilding? #t)])
+
+  (for ([expr-file (directory-list (build-path (current-directory) "exprs"))])
+    (time-suite expr-file)))
